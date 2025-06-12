a94d02f1e37781f758348c201d6306b9bf86e538
AlexNGUEMNIN <alexnguemnin@gmail.com>
2025-06-12 15:49:12 +0100

    feat: add navbar styles and layout for responsive design
    - Implemented SCSS styles for the navbar including header, search, and user profile sections.
    - Added media queries for responsive behavior on smaller screens.
    - Introduced common styles for the main container and page layout.
    
    style: define preset color variables and common styles
    
    - Created a new SCSS file for preset styles including color variables.
    - Established styles for sidebar navigation and active states.
    
    style: enhance global styles and utility classes
    
    - Defined global styles for body, headings, buttons, and cards.
    - Added utility classes for margin, padding, and text alignment.
    
    chore: configure TypeScript settings
    
    - Added TypeScript configuration files for application and project settings.
    - Ensured strict type checking and module resolution for better code quality.

--- Back_end/aps_monitoring_back_end/src/main/java/com/monitoring/authservice/model/AuthResponse.java
+++
@@ -1,6 +0,0 @@
-package com.monitoring.authservice.model;
-
-
-import java.util.List;
-
-public record AuthResponse(String token, List<String> groups) {}
--- Back_end/aps_monitoring_back_end/src/main/java/com/monitoring/authservice/model/UserGroup.java
+++
@@ -1,6 +0,0 @@
-package com.monitoring.authservice.model;
-
-public class UserGroup {
-    private int user_id;
-    private int group_id;
-}
